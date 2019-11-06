e52b61aeb65bfb5957a4d0a964ed15d5a19059b7
jeffchoi72 <ihello0720@gmail.com>
2019-11-06 23:59:56 +0900

    feat: color 모음을 추가한다

---
+++ src/components/atoms/color/index.tsx
@@ -0,0 +1,18 @@
+export const white = "#FFF";
+export const black = "#000";
+
+// brand color
+export const brandGreen5 = "#E5FAEC";
+export const brandGreen10 = "#ADDBBE";
+export const brandGreen20 = "#7CBA93";
+export const brandGreen30 = "#4C9968";
+export const brandGreen40 = "#2A6F43";
+export const brandGreen50 = "#285137";
+
+// slate color
+export const slate5 = "#E2E2E2";
+export const slate10 = "#C4C4C4";
+export const slate20 = "#707070";
+export const slate30 = "#494949";
+export const slate40 = "#272727";
+export const slate50 = "#191919";
