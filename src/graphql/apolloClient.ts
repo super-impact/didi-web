b63ee027c4b2ce0c8e4467f3d2f8fee618bdc8de
jeffchoi72 <ihello0720@gmail.com>
2019-12-22 21:33:04 +0900

    feat: apolloClient가 인증을 할 수 있가 apolloAuthLink를 추가한다

---
+++ src/graphql/apolloClient.ts
@@ -0,0 +1,31 @@
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { setContext } from 'apollo-link-context';
+import { createHttpLink } from 'apollo-link-http';
+import { getAccessToken } from 'utils/authTokenStore';
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8000/graphql"
+});
+
+const authLink = setContext((_, { headers }) => {
+  const accessToken = getAccessToken();
+
+  if (accessToken) {
+    return {
+      headers: {
+        ...headers,
+        "access-token": accessToken || ""
+      }
+    };
+  }
+
+  return {
+    headers
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
