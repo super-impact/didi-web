3732eefd0f5e23bc6f139b9e5863ecea0c860753
jeffchoi72 <ihello0720@gmail.com>
2019-12-22 22:29:21 +0900

    refactor: browserStore 라는 이름으로 browserStore 함수들을 쓰게 export한다

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import * as authTokenStore from './authTokenStore';
 import * as BreakPoints from './breakPoints';
+import * as browserStore from './browserStore';
 
-export { BreakPoints, authTokenStore };
-export * from "./browserStore";
+export { BreakPoints, authTokenStore, browserStore };
