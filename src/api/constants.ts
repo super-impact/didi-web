31e755a0120d9aceff08c581f8c17a5acc34f964
jeffchoi72 <ihello0720@gmail.com>
2019-12-22 22:38:18 +0900

    refactor: graphql과 rest api에 endpoint 값들을 리액트 환경변수를 사용하도록 바꾼다

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,3 @@
-const { REACT_APP_API_URL } = process.env;
+const { REACT_APP_REST_API_URL } = process.env;
 
-export const GOOGLE_AUTH_URL = `${REACT_APP_API_URL}/auth/google`;
+export const GOOGLE_AUTH_URL = `${REACT_APP_REST_API_URL}/auth/google`;
