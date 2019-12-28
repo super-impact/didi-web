6a210be16066145c70b0415e9217e8b42016fe5e
jeffchoi72 <ihello0720@gmail.com>
2019-12-28 21:33:19 +0900

    refactor: MainTemplate, PostDetailTemplate에 헤더 레이아웃을 변경한다

--- src/components/templates/MainTemplate.tsx
+++ src/components/templates/MainTemplate.tsx
@@ -27,9 +27,7 @@ const Layout = styled.div`
 const Header = styled.header`
   width: 100%;
   background-color: white;
-  padding: 12px 16px;
-  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.05);
-  border-bottom: 1px solid #e8e8e8;
+  padding: 14px 16px;
 `;
 
 const ContentSection = styled.div`
