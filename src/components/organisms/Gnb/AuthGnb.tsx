d96408fe0e6466603fa7dbe170e2a4b981a5749c
jeffchoi72 <ihello0720@gmail.com>
2019-12-29 12:12:04 +0900

    feat: AuthGnb 컴포넌트를 만든다

---
+++ src/components/organisms/Gnb/AuthGnb.tsx
@@ -0,0 +1,46 @@
+import Caption1 from 'components/atoms/Typography/Caption1';
+import { UserSessionFragmentFields } from 'graphql/fragments/__generated__/UserSessionFragmentFields';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import UserProfile from '../Profile/UserProfile';
+
+interface Props {
+  userSession: UserSessionFragmentFields;
+}
+
+const AuthGnb: React.FC<Props> = props => {
+  return (
+    <Container>
+      <StyledLink to="/share">
+        <Item>공유하기</Item>
+      </StyledLink>
+      <StyledLink to="/me/bookmark">
+        <Item>내 북마크</Item>
+      </StyledLink>
+      <StyledLink to="/me">
+        <UserProfile userSession={props.userSession} />
+      </StyledLink>
+    </Container>
+  );
+};
+
+export default React.memo(AuthGnb);
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
+
+const Item = styled(Caption1)`
+  font-size: 14px;
+  margin-right: 12px;
+  color: #272727;
+  cursor: pointer;
+`;
---
+++ src/components/organisms/Gnb/index.ts
@@ -0,0 +1,5 @@
+import AuthGnb from './AuthGnb';
+
+export default {
+  AuthGnb
+};
